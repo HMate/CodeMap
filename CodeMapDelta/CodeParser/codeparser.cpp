c8241946547da4c916a4c864985d4862687aed2e
HMate <mhidvegi@gmail.com>
2018-03-14 15:21:31 +0100

    Link to clang with cmake using find_library

--- CodeMapDelta/CodeParser/codeparser.cpp
+++ CodeMapDelta/CodeParser/codeparser.cpp
@@ -1,6 +1,9 @@
 #include "codeparser.h"
 
+#include "clang/Tooling/Tooling.h"
+
 QString CodeParser::parseCode(const QString& source)
 {
+    clang::tooling::ClangTool tool();
     return "dddd";
 }
