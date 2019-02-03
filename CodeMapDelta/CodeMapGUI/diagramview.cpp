d23b966d5f4c8f19c554282f0a924a84d2a1d21b
HMate <mhidvegi@gmail.com>
2019-02-03 16:18:04 +0100

    auto size box based on content size

--- CodeMapDelta/CodeMapGUI/diagramview.cpp
+++ CodeMapDelta/CodeMapGUI/diagramview.cpp
@@ -9,10 +9,13 @@
 class BoxDGI : public QGraphicsItem
 {
     QString m_name;
+    QFont m_font;
+
 public:
     BoxDGI(const QString& name, QGraphicsItem* parent = nullptr) : QGraphicsItem(parent), m_name(name) 
     {
         setFlags(QGraphicsItem::ItemIsFocusable | QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
+        m_font = QFont("Helvetica");
     }
 
 protected:
@@ -20,16 +23,32 @@ protected:
         const QStyleOptionGraphicsItem *option, 
         QWidget *widget = nullptr) override
     {
+        // TODO: The actually rendered font is not Helvetica. Find out what happens.
+        painter->setFont(m_font);
+        auto fmetric = painter->fontMetrics();
+
         painter->setBrush(QBrush(QColor(200, 200, 250)));
         if(isSelected())
             painter->setBrush(QBrush(QColor(200, 250, 250)));
-        painter->drawRoundedRect(0, 0, 50, 50, 10, 10);
-        painter->drawText(QRect(20, 10, 30, 40), m_name);
+
+        const int margin = 10;
+        const int w = fmetric.width(m_name);
+        const int h = fmetric.height();
+
+        auto rectSize = boundingRect();
+        painter->drawRect(rectSize);
+
+        auto textMargin = (rectSize.width() - w) / 2.0;
+        painter->drawText(textMargin, margin+h, m_name);
     }
 
     QRectF boundingRect() const override
     {
-        return QRectF(0, 0, 50, 50);
+        QFontMetrics fmetric(m_font);
+        const int margin = 10;
+        const int w = fmetric.width(m_name);
+        const int h = fmetric.height();
+        return QRectF(0, 0, 2 * margin + w, 2 * margin + h);
     }
 };
 
